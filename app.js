6a36e935a09ca0425127ae3eb062da3254c80a33
beckybp <becky.pugh@hotmail.co.uk>
2023-02-21 08:42:56 +0000

    removing express.json() in line with pr feedback

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const { getCategories } = require('./controllers/games-conroller.js');
 const { handle500Status } = require('./controllers/error-handling-controller.js');
 
 const app = express();
-app.use(express.json());
 
 app.get('/api/categories', getCategories);
 
